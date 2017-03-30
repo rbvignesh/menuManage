9bbd6db20f544a56dd37d87d3ac28c23bf31b89a
Vignesh <Vignesh@Vignesh>
2017-03-30 10:17:38 +0530

    Initial Version

---
+++ src/main/java/com/robustroot/MenuManApplication.java
@@ -0,0 +1,12 @@
+package com.robustroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuManApplication.class, args);
+	}
+}
